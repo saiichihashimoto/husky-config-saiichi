554f8a527dfde2762c7434b2be67052a0a39b226
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-08-19 11:04:56 -0700

    fix(staged): print task output from lint-staged

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	hooks: {
-		'pre-commit': `lint-staged --config ${path.join(__dirname, 'lint-staged.config.js')}`,
+		'pre-commit': `lint-staged --verbose --config ${path.join(__dirname, 'lint-staged.config.js')}`,
 		'pre-push':   `lint-prepush --config ${path.join(__dirname, 'lintprepush.config.js')}`,
 	},
 };
