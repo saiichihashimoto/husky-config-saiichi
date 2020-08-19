cae88a0fc82aa21e915c4f2cced57baaad8c1733
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-08-19 00:08:43 -0700

    fix(staged): show linters in a different order

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -6,20 +6,20 @@ const { build, eslint, stylelint, stylelintSCSS, test } = require('./utils');
 const pkg = require(path.join(process.cwd(), 'package.json'));
 
 module.exports = {
-	'*.{js,ts,tsx}':   eslint(pkg).map((cmd) => `${cmd} --fix`),
-	'*.css':           stylelint(pkg).map((cmd) => `${cmd} --fix`),
-	'*.scss':          stylelintSCSS(pkg).map((cmd) => `${cmd} --fix`),
-	'**/package.json': [
-		'sort-package-json',
+	'*': () => [] // needs to be function to ignore filenames
+		.concat(test(pkg))
+		.concat(build(pkg)),
+	'*.{js,ts,tsx}':            eslint(pkg).map((cmd) => `${cmd} --fix`),
+	'*.css':                    stylelint(pkg).map((cmd) => `${cmd} --fix`),
+	'*.scss':                   stylelintSCSS(pkg).map((cmd) => `${cmd} --fix`),
+	'*.{png,jpeg,jpg,gif,svg}': [
+		'imagemin-lint-staged',
 	],
 	'**/!(package|package-lock).json': [
 		'fixjson --write',
 		'jsonlint --quiet',
 	],
-	'*.{png,jpeg,jpg,gif,svg}': [
-		'imagemin-lint-staged',
+	'**/package.json': [
+		'sort-package-json',
 	],
-	'*': () => [] // needs to be function to ignore filenames
-		.concat(test(pkg))
-		.concat(build(pkg)),
 };
