dde8e5af80a5730139c4b701d5f396dc963a874c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2020-12-23 19:11:07 -0600

    fix(prepush): remove lint-prepush
    wasn't being used and I think huskyrc can do it directly anyways

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,16 @@
 const path = require('path');
 
+const { build, test } = require('./utils');
+
+// eslint-disable-next-line import/no-dynamic-require -- Need to use the user's package.json, not mine
+const pkg = require(path.join(process.cwd(), 'package.json'));
+
 module.exports = {
 	hooks: {
 		'pre-commit': `lint-staged --config ${path.join(__dirname, 'lint-staged.config.js')}`,
-		'pre-push':   `lint-prepush --config ${path.join(__dirname, 'lintprepush.config.js')}`,
+		'pre-push':   []
+			.concat(test(pkg))
+			.concat(build(pkg))
+			.join(' && '),
 	},
 };
